a513349ff79a542184b14262032dce419b5929b8
hevinhsu <hevinhsu@gmail.com>
2025-09-10 09:34:11 +0800

    no message

--- hadoop-ozone/s3gateway/src/main/java/org/apache/hadoop/ozone/s3/endpoint/EndpointBase.java
+++ hadoop-ozone/s3gateway/src/main/java/org/apache/hadoop/ozone/s3/endpoint/EndpointBase.java
@@ -452,7 +452,7 @@ public abstract class EndpointBase implements Auditor {
       Map<String, String> auditMap) {
     auditMap.put("x-amz-request-id", requestIdentifier.getRequestId());
     auditMap.put("x-amz-id-2", requestIdentifier.getAmzId());
-
+    
     AuditMessage.Builder builder = new AuditMessage.Builder()
         .forOperation(op)
         .withParams(auditMap);
