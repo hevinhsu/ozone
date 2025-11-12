b925e7fe59921c0dab8bce7d7c35a48248cb3632
hevinhsu <hevinhsu@gmail.com>
2025-11-12 16:04:38 +0800

    Add apache 2.0 licence

--- hadoop-ozone/dist/src/main/smoketest/s3/presigned_url_helper.py
+++ hadoop-ozone/dist/src/main/smoketest/s3/presigned_url_helper.py
@@ -1,4 +1,18 @@
-# Defer heavy imports so that simple keywords (like Hello World) work without boto3 installed.
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 def generate_presigned_put_object_url(
     aws_access_key_id=None,
@@ -14,7 +28,6 @@ def generate_presigned_put_object_url(
   Generate a presigned URL for PUT Object. This function creates the S3 client internally.
   """
   try:
-    # Import here to avoid requiring boto3 for simple keywords/tests
     import boto3
 
     client_args = {
